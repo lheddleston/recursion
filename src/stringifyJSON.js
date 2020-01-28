d5434c4ae9a91e2070e6cf683bacec09e1acc497
Laura Heddleston <laura.heddleston@gmail.com>
2020-01-27 16:34:58 -0800

    Added logic to stringifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,47 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // should convert input into string
+
+  // if obj is null
+  if (obj === null) {
+  	return 'null';
+  }
+  // if obj is true or false
+  else if (obj === "boolean") {
+  	return ? 'true' : false;
+  }
+  // if obj is undefined
+  else if (obj === undefined) {
+  	return undefined;
+  }
+  // if obj is string
+  else if (typeof obj === "number") {
+  	return String(obj);
+  }
+  // if array:
+  else if (Array.isArray(obj)) {
+  	var result = [];
+  	if (obj.length > 0) {
+  		obj.forEach(function(element) {
+  			result.push(stringifyJSON(element));
+  		}); 		
+  	}
+  	return "[" + result.toString() + "]";
+  }
+
+  // if object:
+  else if (typeof obj === "object") {
+  	var result = [];
+  	Object.keys(obj).forEach(function(key) {
+  		if (obj[key] !== null) {
+  			result.push(stringifyJSON(element));
+  		}
+  	});	
+  	return "{" + result.toString() + "}";
+  }
+else {
+	return '{}';
+}
+
 };
