254cb0e7ad9f98b378cebbd27855329bd358fdd7
Laura Heddleston <laura.heddleston@gmail.com>
2020-01-27 19:02:57 -0800

    Add logic for getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -2,9 +2,29 @@
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
+// You should use document.body, element.childNodes, and element.classList
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+var result = [];
+
+  function loopElements(element) {
+
+  	var childNode = element.childNodes;
+
+    if (element.classList && element.classList.contains(className)) {
+      result.push(element);
+    }
+
+    if (childNode) {
+      for (var i = 0; i < childNode.length; i++) {
+        loopElements(childNode[i]);
+      }
+    }
+  }
+
+  loopElements(document.body);
+
+return result;
 };
